2e45172a73f69f3a394a71890a17d0cfd63afc06
mitchb95 <mitchell.bonner@gmail.com>
2021-09-04 02:19:56 +0000

    Removing reply on all error, causes bot to crash

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ client.on('interactionCreate', async interaction => {
 	}
 	catch (error) {
 		console.error(error);
-		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
